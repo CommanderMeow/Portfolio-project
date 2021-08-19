a8c25118b7661731e79ada68b814420047fefd92
CommanderMeow <64798854+CommanderMeow@users.noreply.github.com>
2021-08-19 17:00:06 +0100

    new website

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("nav-bar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+$(".hamburger").click(function(){
+  $(".nav__ul").toggle()
+});
